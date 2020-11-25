d8975b446d9205af502de3f3bcab84ac4a18fb87
Dejan <dejan.sheki.lukic@gmail.com>
2020-11-25 14:37:46 +0100

    ...

--- script/main.js
+++ script/main.js
@@ -13,3 +13,23 @@ function fixNav() {
 
 
 window.addEventListener('scroll', fixNav);
+
+
+//hamburger
+
+$(function() {
+
+    $(".bars").on("click", function() {
+        $(".bars").toggleClass('bars-open');
+        $('.nav-menu').slideToggle(600);
+    });
+
+    $('.nav-menu a').on('click', function() {
+        var ww = $(window).width();
+        if (ww < 768) {
+            $('.nav-menu').slideToggle(600);
+            $('.bars-open').toggleClass('bars-open');
+        }
+    });
+
+});
